38f74af9162469ca7e5a4aa72a3a243e8a7f5a6f
Hassaan Abbasi <hassaan.abbasi@ryerson.ca>
2020-05-16 01:53:45 -0400

    Calling intent

--- app/src/main/java/com/mash/recipenator/MainActivity.java
+++ app/src/main/java/com/mash/recipenator/MainActivity.java
@@ -23,6 +23,7 @@ public class MainActivity extends AppCompatActivity
             public void onClick(View view)
             {
                 Intent intent = new Intent(MainActivity.this, AudioActivity.class);
+                startActivity(intent);
             }
         });
 
