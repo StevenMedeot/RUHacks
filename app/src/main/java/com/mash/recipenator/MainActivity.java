13b22abc3ccc6a55fb09efd11df0b669633e667a
Steve-PC\Steve <smedeot@ryerson.ca>
2020-05-16 14:53:49 -0400

    Update MainActivity.java

--- app/src/main/java/com/mash/recipenator/MainActivity.java
+++ app/src/main/java/com/mash/recipenator/MainActivity.java
@@ -6,33 +6,9 @@ import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.Manifest;
-import android.content.ContentValues;
-import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.graphics.Bitmap;
-import android.graphics.drawable.BitmapDrawable;
-import android.net.Uri;
-import android.os.Build;
-import android.os.Bundle;
-import android.provider.MediaStore;
-import android.view.View;
-import android.widget.Button;
-import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
-    /*
-     CAMERA PICTURE TAKING TEST
-
-     */
-    private static final int PERMISSION_CODE = 1000;
-    private static final int IMAGE_CAPTURE_CODE = 1001;
-
-    private Button m_photoButton;
-    private ImageView m_imageView;
-
-    private Uri m_uri;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -55,24 +31,5 @@ public class MainActivity extends AppCompatActivity {
         Button buttonImage = (Button) findViewById(R.id.image_b);
     }
 
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults)
-    {
-
-    }
-
-    @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if (resultCode == RESULT_OK)
-        {
-            m_imageView.setImageURI(m_uri);
-
-            Bitmap b = ((BitmapDrawable)(m_imageView.getDrawable())).getBitmap();
-
-            String output = CloudIntegration.GetFromImage(this, b, 60.0f);
-
-        }
-    }
 
 }
