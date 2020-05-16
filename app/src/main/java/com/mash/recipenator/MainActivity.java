c64b3a3ddebcbfe366cc812439dc70ac71aeb1dc
Hassaan Abbasi <hassaan.abbasi@ryerson.ca>
2020-05-15 23:05:49 -0400

    Real recommit

---
+++ app/src/main/java/com/mash/recipenator/MainActivity.java
@@ -0,0 +1,17 @@
+package com.mash.recipenator;
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity
+{
+    //Let user choose between text, audio, picture
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+}
